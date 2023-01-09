9d0d8ac794d31f9b0149d0ddb492fff2892580f2
siwanijaiswal <siwani.jce20@sot.pdpu.ac.in>
2023-01-09 22:52:44 +0530

    version 1.0

---
+++ main.js
@@ -0,0 +1,23 @@
+const john = document.querySelector(".john");
+const tanya = document.querySelector(".tanya");
+const prev = document.querySelectorAll(".prev_button");
+const next = document.querySelectorAll(".next_button");
+
+john.classList.add("hide");
+
+const slideChange = () => {
+    if (john.classList.contains("hide")) {
+        john.classList.remove("hide");
+        tanya.classList.add("hide");
+    }
+    else if (tanya.classList.contains("hide")) {
+        tanya.classList.remove("hide");
+        john.classList.add("hide");
+    }
+    else {
+        john.classList.add("hide");
+    }
+}
+
+prev.forEach(item => item.addEventListener("click", slideChange));
+next.forEach(item => item.addEventListener("click", slideChange));
